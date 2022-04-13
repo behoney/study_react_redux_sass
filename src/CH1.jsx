5e74c8dfd7e81369bf32fa163cc7a2bc3f142130
behoney <bhoney1016@gmail.com>
2022-04-13 02:16:08 +0000

    [add] 1.5

--- src/CH1.jsx
+++ src/CH1.jsx
@@ -3,16 +3,22 @@ import React from 'react'
 function CH1() {
   const text = "hi,,,,,";
 
-  const Template = ({ title, detail }) => (
+  const Template = ({ title, detail, children }) => (
     <div>
       <h1>{title}</h1>
       <h3>{detail}</h3>
+      {children}
     </div>
   )
 
+  const Test1_5 = ()=><Template title="test1_5" detail={"detail....1_5"}></Template>
+
   return (
     <>
-      <Template title="title,," detail="detail..."></Template>
+      <Template title="title,," detail="detail...">
+        <span>span children</span>
+        <Test1_5></Test1_5>
+      </Template>
     </>
   )
 }
