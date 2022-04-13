5226615b681251555fc586eb42caa6e63030ba17
behoney <bhoney1016@gmail.com>
2022-04-13 21:28:51 +0900

    [add] transition delay

--- src/Transition.js
+++ src/Transition.js
@@ -6,16 +6,36 @@ function Transition() {
         <div>
             <div>1. Transition</div>
             <Ex1 />
+            <Ex2 />
         </div>
     )
 }
 
 const Ex1 = () => {
     return (<>
-        <div className='transition-1'>
-            transition basic
+        <div className="container">
+            <div className='transition-1'>
+                transition basic
+            </div>
         </div>
     </>)
 }
+
+const Ex2 = () => {
+    const Block = ({ delay }) => {
+        const wait = delay.toString() + "s"
+        return (<div className='transition-2' style={{ transitionDelay: wait }}>
+            transition delay: {wait}
+        </div>)
+    }
+    return (
+        <div className="container">
+            <Block delay="0.5"></Block>
+            <Block delay="1"></Block>
+            <Block delay="2"></Block>
+        </div>)
+}
+
+
 
 export default Transition
