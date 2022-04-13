af1e58fac73fd646d4a0de01faeb41689bce988a
behoney <bhoney1016@gmail.com>
2022-04-13 05:58:39 +0000

    [add] 1.13

--- src/CH1.jsx
+++ src/CH1.jsx
@@ -1,24 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function CH1() {
-
-
+  const [test, setTest] = useState('')
   return (
     <>
-      {
-        [1, 2, 3, 4, 5, 6, 7, 8, 9].map(num =>
-          <Number key={num} number={num} select={num === 1}></Number>
-        )
-      }
+      <input onInput={(e) => setTest(e.target.value)} type="text"></input>
+      <div> <p>
+        {test}
+      </p></div>
     </>
   )
 }
 
 export default CH1
-
-const onEvent = () => { }
-
-const Number = ({ number, select }) => {
-
-  return number % 2 === 0 ? <h1 onMouseMove={onEvent} >{number}</h1> : select ? <h3>selected {number}</h3> : <h5>{number}</h5>
-}
