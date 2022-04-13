21bb76458bce35bc7fd8a843fa763dfe40fbe9cf
behoney <bhoney1016@gmail.com>
2022-04-13 02:29:45 +0000

    [add] 1.7

--- src/CH1.jsx
+++ src/CH1.jsx
@@ -2,16 +2,12 @@ import React from 'react'
 
 function CH1() {
 
-  const Number = ({ number, select }) => {
-
-    return number % 2 === 0 ? <h1>{number}</h1> : select ? <h3>selected {number}</h3> : <h5>{number}</h5>
-  }
 
   return (
     <>
       {
         [1, 2, 3, 4, 5, 6, 7, 8, 9].map(num =>
-          <Number number={num} select={num === 1}></Number>
+          <Number key={num} number={num} select={num === 1}></Number>
         )
       }
     </>
@@ -19,3 +15,8 @@ function CH1() {
 }
 
 export default CH1
+
+const Number = ({ number, select }) => {
+
+  return number % 2 === 0 ? <h1>{number}</h1> : select ? <h3>selected {number}</h3> : <h5>{number}</h5>
+}
