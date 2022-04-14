8e2798481126ff555e955dc0dfce97351fb44345
behoney <bhoney1016@gmail.com>
2022-04-14 06:36:49 +0000

    [add] ~3.9

--- src/CH3.jsx
+++ src/CH3.jsx
@@ -1,29 +1,85 @@
-import React, { useState, useEffect, useRef, useReducer } from 'react'
+import React, { useState, useEffect, useRef, useReducer, useMemo } from 'react'
 import "./CH3.scss"
 
 
 
 function CH3() {
     return (<>
-        <Dialog msg="my message"></Dialog>
-        <Dialog msg={messageDiv}></Dialog>
+        <Posts></Posts>
     </>)
 }
 
+const commentList = [
+    { title: "Com", likes: 1, id: 1 },
+    { title: "ment", likes: 1, id: 2 },
+    { title: "ssss", likes: 1, id: 999 }
+]
 
+const Posts = () => {
+    const [list, setList] = useState(commentList)
+    const createPost = () => {
+        setList(
+            [
+                ...list,
+                { title: "temp" + list.length, likes: 0, id: list.length }
+            ]
+        )
+    }
+
+    const likeBtn = (pid) => {
+        const target = list.filter(e => e.id === pid)[0];
+        target.likes += 1;
+        setList(
+            [...list]
+        )
+    }
+
+    useEffect(() => {
+        const itv = setTimeout(() => {
+            // createPost()
+        }, 1000);
+
+        return () => {
+            clearTimeout(itv);
+        }
+    }, [list])
+
+    return (<div>
+        {
+            list.map((e) => {
+                return <Content title={e.title} key={e.id} likes={e.likes} handler={() => likeBtn(e.id)} />
+            })
+        }
+    </div>)
+}
+
+const Content = ({ title, likes, id, handler }) => {
+    return (<div>
+        <span>
+
+            {title} : {likes}
+        </span>
+        <button onClick={handler}>like</button>
+
+    </div>)
+}
 
 export default CH3
 
 
-const messageDiv = (<div
-    onClick={() => {
-        console.log("hi");
-    }}
-    style={{
-        textAlign: "center",
-    }}>
-    hello messageDiv</div>)
 
+
+
+const messageDiv = () => {
+    return (<div
+        onClick={() => {
+            console.log("hi");
+        }}
+        style={{
+            textAlign: "center",
+        }}>
+        hello messageDiv</div>)
+}
 
 const Dialog = (props) => {
     const [open, setOpen] = useState(false)
@@ -47,17 +103,6 @@ const Dialog = (props) => {
     </>)
 
 }
-
-
-
-
-
-
-
-
-
-
-
 
 
 
