decd22ac4c91f0c8fc6999150daf686a49075301
behoney <bhoney1016@gmail.com>
2022-04-14 08:13:02 +0000

    [add] 4.4

--- src/CH4.jsx
+++ src/CH4.jsx
@@ -1,18 +1,43 @@
 import React, { useState, useEffect, useRef, useReducer, useMemo } from 'react'
 import "./CH4.scss"
 import moment from 'moment-timezone'
-
+import { add, compareAsc, format } from "date-fns"
 
 
 function CH4() {
     return (<>
-        <MomentExample />
+        {/* <MomentExample /> */}
+        <FnsExample />
     </>)
 }
 
 
 
 export default CH4;
+
+const FnsExample = () => {
+
+    const dates = [
+        add(new Date("2000-01-02"), { years: 1 }),
+        add(new Date("2000-01-02"), { month: 1 }),
+        add(new Date("2000-01-02"), { minutes: 1 }),
+        add(new Date("2000-01-02"), { seconds: 1 }),
+        add(new Date("2000-01-02"), { day: 1 }),
+
+    ]
+    dates.sort(compareAsc)
+    return (<div>
+        {
+            format(new Date(), "yyyy-mm-dd")
+        }
+        {dates.map(e => {
+            return (<div>
+                {e.toString()}
+            </div>)
+        })}
+    </div>)
+}
+
 const MomentExample = () => {
     const [result, setResult] = useState("")
     const [date, setDate] = useState("")
