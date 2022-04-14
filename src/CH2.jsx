dee902eaea18f9c4b15a65553487f87de64442c3
behoney <bhoney1016@gmail.com>
2022-04-14 02:34:11 +0000

    [add] ~ 2.4

--- src/CH2.jsx
+++ src/CH2.jsx
@@ -1,11 +1,19 @@
 import React, { useState, useEffect, useRef } from 'react'
 import "./CH2.scss"
 
+const Welcome = (props) => {
+    return (<div>
+        hello {props.name}
+    </div>)
+}
 
 function CH2() {
 
     return (<>
-    
+        <Welcome name="A"></Welcome>
+        <Welcome name="B"></Welcome>
+        <Welcome name="C"></Welcome>
+        <Welcome name="D"></Welcome>
     </>)
 }
 
