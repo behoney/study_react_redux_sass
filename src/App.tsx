5adb10a078bb7620ae677ab37c50a0d4e7bd4d62
behoney <bhoney1016@gmail.com>
2022-04-18 06:32:54 +0000

    [add] abstract class

--- src/App.tsx
+++ src/App.tsx
@@ -15,16 +15,12 @@ import WebAnimations from './WebAnimations';
 
 import test from "./ts/class"
 
-test.put({ id: "name", price: 1000 })
-test.put({ id: "name1", price: 2000 })
-test.put({ id: "name2", price: 3000 })
-test.addPromotion();
+test.say("hi")
 
 function App() {
   return (
     <div className="App">
 
-      {test.get("id")}
       {/* <Review></Review> */}
       {/* <CH5 /> */}
       {/* <CH5_Friend></CH5_Friend> */}
--- src/ts/class.ts
+++ src/ts/class.ts
@@ -34,6 +34,45 @@ class PromotionCart extends Cart {
     }
 }
 
-const test: PromotionCart = new PromotionCart({ name: "joe" });
+
+interface Person {
+    name: string;
+    say(msg: string): void;
+}
+
+interface Programmer {
+    writeCode(): void;
+}
+
+abstract class Korean implements Person, Programmer {
+    constructor(public name: string) {
+        this.name = name;
+    }
+    say(msg: string): void {
+        console.log(this.name + " said: " + msg);
+        this.writeCode();
+    }
+
+    writeCode(): void {
+        console.error("write this code");
+    }
+
+    abstract language(): void;
+}
+
+class KoreanProgrammer extends Korean {
+    constructor(name: string, private id: number) {
+        super(name);
+        this.id = id
+        this.language();
+    }
+
+    language(): void {
+        console.log(this.name + " uses JS");
+    }
+
+}
+
+const test: KoreanProgrammer = new KoreanProgrammer("joe", 19291929);
 
 export default test;
