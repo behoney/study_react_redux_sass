ecfa67815ac1d15a2343917e0f9d77802c21c1a9
behoney <bhoney1016@gmail.com>
2022-04-20 08:09:49 +0000

    [add] generic funtion

--- src/ts/class.ts
+++ src/ts/class.ts
@@ -1,39 +1,22 @@
-function helloString(msg: string) {
+type HelloFunctionGeneric1 = <T>(msg: T) => T;
+
+const helloFunctionGeneric1: HelloFunctionGeneric1 = <T>(msg: T): T => {
     return msg;
 }
 
-function helloNumber(msg: number) {
+interface HelloFunctionGeneric2 {
+    <T>(message: T): T
+}
+
+const helloFunctionGeneric2: HelloFunctionGeneric2 = <T>(msg: T): T => {
     return msg;
-}
-
-function helloGeneric<T>(msg: T) {
-    return msg;
-}
-
-function genericCompare<T, U>(a: T, b: U): boolean {
-    return typeof a === typeof b
-}
-
-function helloBasic<T, U>(msg: T, cmt: U) {
-    return msg
-}
-
-function helloArray<T>(message: T[]): T {
-    return message[0];
-}
-
-function helloTuple<T, K>(message: [T, K]): T {
-    return message[0]
 }
 
 const test = () => {
+    console.log(
 
-    console.log(helloArray([1, 2, 3, 4, 12, 1]));
-    console.log(helloArray<number[]>([[12], [12]]));
-
-    console.log(helloTuple([[1212333], [12]]));
-
-
+        helloFunctionGeneric1(1)
+    );
 }
 
 export default test;
