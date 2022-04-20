50521339d201374499a9d84725a13b6922a4544e
behoney <bhoney1016@gmail.com>
2022-04-20 07:53:41 +0000

    [add] abstract class

--- src/ts/class.ts
+++ src/ts/class.ts
@@ -1,45 +1,22 @@
-class Parent {
-    constructor(protected _name: string, private _age: number) {
+abstract class AbsPerson {
+    protected _name: string = "mark";
 
-    }
-    public print(): void {
-        console.log(this._name, this._age);
+    abstract setName(name: string): void;
 
-    }
-
-    protected printName(): void {
-        console.log(this._name, this._age);
-    }
 }
-
-class Child extends Parent {
-
-
-    public gender: string = "male";
-
-    constructor(age: number) {
-        super("child name", age);
-        this.printName();
-    }
-
-    print() {
-        super.print();
-        console.log(this.gender);
-
+class Person extends AbsPerson {
+    setName(name: string): void {
+        this._name = name;
     }
 
 }
-
-
 const test = () => {
-    const parent = new Parent("p", 1);
-    parent.print();
 
-    const c = new Child(2);
-    c.print();
-
-
-
+    const a = new Person();
+    console.log(a);
+    a.setName("hi");
+    console.log(a);
+    
 }
 
 export default test;
