1577cb67ee1913ca81c0138b955ce6c7786221ec
behoney <bhoney1016@gmail.com>
2022-04-20 08:06:04 +0000

    [add] generic array & tuple

--- src/ts/class.ts
+++ src/ts/class.ts
@@ -14,17 +14,24 @@ function genericCompare<T, U>(a: T, b: U): boolean {
     return typeof a === typeof b
 }
 
+function helloBasic<T, U>(msg: T, cmt: U) {
+    return msg
+}
+
+function helloArray<T>(message: T[]): T {
+    return message[0];
+}
+
+function helloTuple<T, K>(message: [T, K]): T {
+    return message[0]
+}
+
 const test = () => {
-    console.log(
-        helloString("a"),
-        helloNumber(1),
-        helloGeneric(1),
-        genericCompare(1, '1'),
-        genericCompare(1, 2222),
-        genericCompare(1, {}),
-        genericCompare(1, Infinity),
-        genericCompare(NaN, '1')
-    );
+
+    console.log(helloArray([1, 2, 3, 4, 12, 1]));
+    console.log(helloArray<number[]>([[12], [12]]));
+
+    console.log(helloTuple([[1212333], [12]]));
 
 
 }
